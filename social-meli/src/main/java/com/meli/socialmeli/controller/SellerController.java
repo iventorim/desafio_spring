2c435cec2815fa96d112941e0337d67934bcc67c
iventorim <igor.ventorim@gmail.com>
2021-07-07 14:11:11 -0300

    Resolvendo US0002
    Permitindo obter o número de clientes seguidores de um determinado vendedor

--- social-meli/src/main/java/com/meli/socialmeli/controller/SellerController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/SellerController.java
@@ -1,7 +1,27 @@
 package com.meli.socialmeli.controller;
 
+import com.meli.socialmeli.dto.FollowersSellerDTO;
+import com.meli.socialmeli.service.SellerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SellerController {
+
+    private final SellerService sellerService;
+
+    public SellerController(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    @GetMapping("/users/{userId}/followers/count")
+    @ResponseStatus(value = HttpStatus.OK)
+    public FollowersSellerDTO getFollowersSellerCount(@PathVariable Integer userId) {
+
+        return sellerService.getFollowersSellerCount(userId);
+    }
+
 }
--- social-meli/src/main/java/com/meli/socialmeli/service/SellerService.java
+++ social-meli/src/main/java/com/meli/socialmeli/service/SellerService.java
@@ -1,4 +1,33 @@
 package com.meli.socialmeli.service;
 
+import com.meli.socialmeli.dto.FollowersSellerDTO;
+import com.meli.socialmeli.entity.Seller;
+import com.meli.socialmeli.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
 public class SellerService {
+
+    final SellerRepository sellerRepository;
+
+    @Autowired
+    public SellerService(SellerRepository sellerRepository) {
+        this.sellerRepository = sellerRepository;
+    }
+
+    public FollowersSellerDTO getFollowersSellerCount(Integer sellerId) {
+
+        Optional<Seller> optionalSeller = sellerRepository.findById(sellerId);
+
+        if(optionalSeller.isPresent()) {
+            return FollowersSellerDTO.convert(optionalSeller.get());
+        }
+
+        throw new NoSuchElementException("Não foi encontrado nenhum usuário vendedor com o id: "+sellerId);
+    }
+
 }
