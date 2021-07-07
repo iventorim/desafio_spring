eaeffaaee8d1fd03a53cd6a7acc6edc23c671d68
CaioMF <caio.fagundes@mercadolivre.com>
2021-07-07 15:01:33 -0300

    Criando a rota e adicionado o PostFom para receber os dados do payload da tabela Post

--- social-meli/src/main/java/com/meli/socialmeli/controller/PostController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/PostController.java
@@ -1,7 +1,26 @@
 package com.meli.socialmeli.controller;
 
+import com.meli.socialmeli.entity.Post;
+import com.meli.socialmeli.form.PostForm;
+import com.meli.socialmeli.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/products/newpost")
+    public void save(@RequestBody PostForm postForm){
+        postService.save(postForm);
+    }
+
 }
