e2449140b99010c9fbf2ac1eb03e9872904ce94d
Arthur Massaini <arthur.massaini@mercadolivre.com>
2021-07-07 11:44:50 -0300

    Criando funcao do service de cliente para encontrar quem esta seguindo por id

--- social-meli/src/main/java/com/meli/socialmeli/service/ClientService.java
+++ social-meli/src/main/java/com/meli/socialmeli/service/ClientService.java
@@ -1,4 +1,28 @@
 package com.meli.socialmeli.service;
 
+import com.meli.socialmeli.entity.Client;
+import com.meli.socialmeli.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
 public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client findById(Integer UserID) {
+        return clientRepository
+                .findById(UserID)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("UserId " +UserID + " não encontrado"));
+    }
+
 }
