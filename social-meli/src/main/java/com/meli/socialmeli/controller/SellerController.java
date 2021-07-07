4463d2966f1a211d46d2650642b25c8270aba884
CaioMF <caio.fagundes@mercadolivre.com>
2021-07-07 15:47:53 -0300

    Adicionando rota para retornar os seguidores de um vendedor

--- social-meli/src/main/java/com/meli/socialmeli/controller/SellerController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/SellerController.java
@@ -1,7 +1,27 @@
 package com.meli.socialmeli.controller;
 
+import com.meli.socialmeli.dto.FollowersDTO;
+import com.meli.socialmeli.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SellerController {
+
+    private final SellerService sellerService;
+
+
+    @Autowired
+    public SellerController(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    @GetMapping("/users/{userID}/followers/list")
+    public FollowersDTO followersList(@PathVariable int userID){
+        return sellerService.getFollowers(userID);
+    }
 }
