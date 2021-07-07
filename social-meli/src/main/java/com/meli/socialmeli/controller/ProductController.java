b92620dc5a873e463fd8bab859742446eaf1ef08
Vitor Vasconcellos <vitor.vasconcellos@mercadolivre.com>
2021-07-07 14:01:30 -0300

    ProductController - Adicionado rota para obter posts de quem um usuário segue

--- social-meli/src/main/java/com/meli/socialmeli/controller/ProductController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/ProductController.java
@@ -1,7 +1,32 @@
 package com.meli.socialmeli.controller;
 
+import com.meli.socialmeli.dto.UserFollowingPostsDTO;
+import com.meli.socialmeli.service.ClientService;
+import com.meli.socialmeli.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService service;
+    private final ClientService clientService;
+
+    @Autowired
+    public ProductController(ProductService service, ClientService clientService) {
+        this.service = service;
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/followed/{userId}/list")
+    @ResponseStatus(HttpStatus.OK)
+    public UserFollowingPostsDTO listSellersPostsByUserId(@PathVariable int userId) {
+        return new UserFollowingPostsDTO(userId, clientService.getFollowingPosts(userId));
+    }
 }
