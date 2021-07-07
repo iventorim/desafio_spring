a098ca53fea73c7a4cb502d80093d5eea08a4d46
CaioMF <caio.fagundes@mercadolivre.com>
2021-07-07 15:52:44 -0300

    Adicionando ao SellerService o método que retorna pelo id, apenas o vendedor  e seus seguidores

--- social-meli/src/main/java/com/meli/socialmeli/service/SellerService.java
+++ social-meli/src/main/java/com/meli/socialmeli/service/SellerService.java
@@ -1,4 +1,26 @@
 package com.meli.socialmeli.service;
 
+import com.meli.socialmeli.dto.FollowersDTO;
+import com.meli.socialmeli.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SellerService {
+
+    private final SellerRepository sellerRepository;
+
+    @Autowired
+    public SellerService(SellerRepository sellerRepository) {
+        this.sellerRepository = sellerRepository;
+    }
+
+
+    public FollowersDTO getFollowers(Integer userID) {
+
+        return FollowersDTO.convert(sellerRepository.getById(userID));
+
+    }
 }
