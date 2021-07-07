ff91f0b945b7b69c9d4c580366784f4134b47cdb
Arthur Massaini <arthur.massaini@mercadolivre.com>
2021-07-07 11:43:52 -0300

    Criando funcao do controller para endpoint de quem estou seguindo

--- social-meli/src/main/java/com/meli/socialmeli/controller/ClientController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/ClientController.java
@@ -1,12 +1,30 @@
 package com.meli.socialmeli.controller;
 
+import com.meli.socialmeli.entity.Client;
+import com.meli.socialmeli.repository.ClientRepository;
+import com.meli.socialmeli.service.ClientService;
 import com.meli.socialmeli.service.CreatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/users/{UserID}/followed/list")
+    @ResponseStatus(HttpStatus.OK)
+    private Client whoAmIFollowing(@PathVariable Integer UserID) {
+        return clientService.findById(UserID);
+    }
 
 }
