023f1584ab8a7acf9f6792b60df4f79f91c0e4bc
Vitor Vasconcellos <vitor.vasconcellos@mercadolivre.com>
2021-07-07 14:04:17 -0300

    Adicionado DTO e Service para buscar publicações dos Sellers que um usuário segue

--- social-meli/src/main/java/com/meli/socialmeli/service/ClientService.java
+++ social-meli/src/main/java/com/meli/socialmeli/service/ClientService.java
@@ -1,4 +1,38 @@
 package com.meli.socialmeli.service;
 
+import com.meli.socialmeli.entity.Client;
+import com.meli.socialmeli.entity.Post;
+import com.meli.socialmeli.entity.Seller;
+import com.meli.socialmeli.exception.ClientNotFoundException;
+import com.meli.socialmeli.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ClientService {
+
+    private final ClientRepository repository;
+
+    @Autowired
+    public ClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> getFollowingPosts(int userId) {
+        Client client = repository.findById(userId)
+                .orElseThrow(() -> new ClientNotFoundException("Cliente " + userId + " não encontrado."));
+
+        List<Seller> following = client.getFollowing();
+        List<Post> postList = new ArrayList<>();
+
+        following
+                .stream()
+                .map(Seller::getPosts)
+                .forEach(postList::addAll);
+
+        return postList;
+    }
 }
