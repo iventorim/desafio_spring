4220fc44f79b90a7f16f4dd822dce41bf05ec7a7
iventorim <igor.ventorim@gmail.com>
2021-07-08 16:36:26 -0300

    Fix annotation RestController removida no merge

--- social-meli/src/main/java/com/meli/socialmeli/controller/PostController.java
+++ social-meli/src/main/java/com/meli/socialmeli/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PostController {
 
     private final PostService postService;
