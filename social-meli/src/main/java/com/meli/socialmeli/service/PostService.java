03e9d22107f76ddf70ec86d915f584426b00f86c
CaioMF <caio.fagundes@mercadolivre.com>
2021-07-08 14:12:46 -0300

    Alterando para salvar produto de um post se ele não existir e pegar a referencia caso exista

--- social-meli/src/main/java/com/meli/socialmeli/service/PostService.java
+++ social-meli/src/main/java/com/meli/socialmeli/service/PostService.java
@@ -1,8 +1,10 @@
 package com.meli.socialmeli.service;
 
+import com.meli.socialmeli.entity.Product;
 import com.meli.socialmeli.entity.Seller;
 import com.meli.socialmeli.form.PostForm;
 import com.meli.socialmeli.repository.PostRepository;
+import com.meli.socialmeli.repository.ProductRepository;
 import com.meli.socialmeli.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,13 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final SellerRepository sellerRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository, SellerRepository sellerRepository) {
+    public PostService(PostRepository postRepository, SellerRepository sellerRepository, ProductRepository productRepository) {
         this.postRepository = postRepository;
         this.sellerRepository = sellerRepository;
+        this.productRepository = productRepository;
     }
 
     public void save(PostForm postForm){
@@ -26,6 +30,11 @@ public class PostService {
         Seller seller = sellerRepository.findById(postForm.getUserId())
                 .orElseThrow(() -> new NoSuchElementException("Vendedor com id: " + postForm.getUserId() + ", não encontrado"));
         postForm.setSeller(seller);
+
+        Product product = productRepository.findById(postForm.getDetail().getProductId())
+                .orElse(productRepository.save(postForm.getDetail()));
+        postForm.setDetail(product);
+
         postRepository.save(PostForm.convert(postForm));
 
     }
