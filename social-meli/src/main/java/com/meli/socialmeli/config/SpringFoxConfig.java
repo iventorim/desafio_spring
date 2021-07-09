f27f935e8a3c3522153890af088b9e8d594f3ea0
iventorim <igor.ventorim@gmail.com>
2021-07-09 12:46:06 -0300

    Adicionando configuração da documentação da API

--- social-meli/src/main/java/com/meli/socialmeli/config/SpringFoxConfig.java
+++ social-meli/src/main/java/com/meli/socialmeli/config/SpringFoxConfig.java
@@ -2,10 +2,17 @@ package com.meli.socialmeli.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Response;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 public class SpringFoxConfig {
@@ -14,8 +21,29 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.meli.socialmeli.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, this.getListPossibleErrors())
+                .globalResponses(HttpMethod.POST, this.getListPossibleErrors());
+    }
+
+    private List<Response> getListPossibleErrors() {
+        return List.of(
+                new ResponseBuilder().code("400").description("Bad request").build(),
+                new ResponseBuilder().code("404").description("Not Found").build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Social Meli")
+                .description("Desafio Spring Wave 02-Brasil - API Rest Social Meli")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
     }
 
